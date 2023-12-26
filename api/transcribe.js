735a32054cd6f820306a64bd5a3dc21589dec7ce
KingAmada <sarkiamada@gmail.com>
2023-12-26 02:41:02 -0800

    Update transcribe.js

--- api/transcribe.js
+++ api/transcribe.js
@@ -2,58 +2,42 @@ import multer from 'multer';
 import FormData from 'form-data';
 import fetch from 'node-fetch';
 
-// Multer configuration
+// Multer configuration for handling 'multipart/form-data'
 const upload = multer({ storage: multer.memoryStorage() });
 
-export default upload.single('audio'), async function handler(req, res) {
-    if (req.method === 'POST') {
-        try {
-            const formData = new FormData();
-            formData.append('file', req.file.buffer, req.file.originalname);
-            formData.append('model', 'whisper-1');
+export default function handler(req, res) {
+    // Apply multer middleware to process the file
+    upload.single('audio')(req, res, async (err) => {
+        if (err) {
+            return res.status(500).send('Error processing file');
+        }
 
-            const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Bearer ${process.env.WHISPER}`,
-                    // Content-Type is set automatically by FormData
-                },
-                body: formData
-            });
+        if (req.method === 'POST') {
+            try {
+                const formData = new FormData();
+                formData.append('file', req.file.buffer, req.file.originalname);
+                formData.append('model', 'whisper-1');
 
-            if (!response.ok) {
-                throw new Error(`Error from Whisper API: ${response.statusText}`);
+                const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': `Bearer ${process.env.WHISPER}`,
+                    },
+                    body: formData
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Error from Whisper API: ${response.statusText}`);
+                }
+
+                const data = await response.json();
+                res.status(200).json({ text: data.text });
+            } catch (error) {
+                console.error('Error:', error);
+                res.status(500).send('Internal Server Error');
             }
-
-            const data = await response.json();
-            res.status(200).json({ text: data.text });
-        } catch (error) {
-            console.error('Error:', error);
-            res.status(500).send('Internal Server Error');
+        } else {
+            res.status(405).send('Method Not Allowed');
         }
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
-};
-
-
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        // Assuming the audio data is received as a blob in the request body
-        const audioBlob = req.body; 
-
-        try {
-            const transcribedText = await callWhisperAPI(audioBlob);
-            res.status(200).json({ text: transcribedText });
-        } catch (error) {
-            console.error('Error in transcribe function:', error);
-            res.status(500).send('Internal Server Error');
-        }
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
+    });
 }
-
-
-
-
