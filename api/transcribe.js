c059cd940de6dc2d6ac71266eee54a73490a2d61
KingAmada <sarkiamada@gmail.com>
2023-12-25 18:44:28 -0800

    Update transcribe.js

--- api/transcribe.js
+++ api/transcribe.js
@@ -25,14 +25,16 @@ async function callWhisperAPI(audioBlob) {
     try {
         const url = 'https://api.openai.com/v1/audio/transcriptions';
         const formData = new FormData();
-const audioFile = new File([audioBlob], "audio.mp4", { type: "audio/mp4" });
-formData.append('file', audioFile);
+
+        // Append the audio data as a blob
+        formData.append('file', audioBlob, "audio.mp4");
         formData.append('model', 'whisper-1');
 
         const response = await fetch(url, {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${process.env.WHISPER}`
+                // FormData will set the Content-Type to 'multipart/form-data'
             },
             body: formData
         });
@@ -48,6 +50,24 @@ formData.append('file', audioFile);
         throw error;
     }
 }
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        // Assuming the audio data is received as a blob in the request body
+        const audioBlob = req.body; 
+
+        try {
+            const transcribedText = await callWhisperAPI(audioBlob);
+            res.status(200).json({ text: transcribedText });
+        } catch (error) {
+            console.error('Error in transcribe function:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
+}
+
 
 
 
