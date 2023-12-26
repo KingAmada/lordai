5cda8346d6e7dba211db242caae039d007508073
KingAmada <sarkiamada@gmail.com>
2023-12-25 18:46:58 -0800

    Update transcribe.js

--- api/transcribe.js
+++ api/transcribe.js
@@ -1,23 +1,3 @@
-
-// pages/api/transcribe.js
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        try {
-            const audioData = req.body; // Audio data received from the frontend
-
-            // Call the Whisper API (ensure you have proper authentication)
-            const transcribedText = await callWhisperAPI(audioData);
-
-            res.status(200).json({ text: transcribedText });
-        } catch (error) {
-            console.error('Error in transcribe function:', error);
-            res.status(500).send('Internal Server Error');
-        }
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
-}
-
 import fetch from 'node-fetch';
 import FormData from 'form-data';
 
